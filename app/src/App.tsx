23b50e914ec7b7721ccf3b8cc8ca75834e02ce9a
mafortin <mafortin@student.42quebec.com>
2023-01-27 19:01:52 -0500

    Added MenuIcon

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,5 +1,128 @@
+import NavBar from "./components/common/NavBar";
+
 function App() {
-  return <div>test</div>;
+  return (
+    <div>
+      <NavBar />
+      <div>
+        First line Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+        Amet ut doloremque facere modi mollitia adipisci voluptas eaque error
+        animi alias corrupti, aliquid officia natus perspiciatis nam vero.
+        Voluptas, excepturi sapiente. Lorem ipsum dolor sit amet consectetur,
+        adipisicing elit. Porro unde voluptates facere sint laboriosam beatae
+        sequi ipsum magni distinctio ex eaque hic illum dolorum, quod quae
+        delectus architecto natus qui! Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Explicabo iure nihil quia odio quam quaerat
+        voluptatibus dignissimos nemo molestias eaque rem, sit molestiae
+        cupiditate quae quisquam doloremque in culpa. Perferendis? Lorem ipsum
+        dolor sit amet, consectetur adipisicing elit. Amet ut doloremque facere
+        modi mollitia adipisci voluptas eaque error animi alias corrupti,
+        aliquid officia natus perspiciatis nam vero. Voluptas, excepturi
+        sapiente. Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+        Porro unde voluptates facere sint laboriosam beatae sequi ipsum magni
+        distinctio ex eaque hic illum dolorum, quod quae delectus architecto
+        natus qui! Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Explicabo iure nihil quia odio quam quaerat voluptatibus dignissimos
+        nemo molestias eaque rem, sit molestiae cupiditate quae quisquam
+        doloremque in culpa. Perferendis? Lorem ipsum dolor sit amet,
+        consectetur adipisicing elit. Amet ut doloremque facere modi mollitia
+        adipisci voluptas eaque error animi alias corrupti, aliquid officia
+        natus perspiciatis nam vero. Voluptas, excepturi sapiente. Lorem ipsum
+        dolor sit amet consectetur, adipisicing elit. Porro unde voluptates
+        facere sint laboriosam beatae sequi ipsum magni distinctio ex eaque hic
+        illum dolorum, quod quae delectus architecto natus qui! Lorem ipsum
+        dolor sit amet consectetur adipisicing elit. Explicabo iure nihil quia
+        odio quam quaerat voluptatibus dignissimos nemo molestias eaque rem, sit
+        molestiae cupiditate quae quisquam doloremque in culpa. Perferendis?
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet ut
+        doloremque facere modi mollitia adipisci voluptas eaque error animi
+        alias corrupti, aliquid officia natus perspiciatis nam vero. Voluptas,
+        excepturi sapiente. Lorem ipsum dolor sit amet consectetur, adipisicing
+        elit. Porro unde voluptates facere sint laboriosam beatae sequi ipsum
+        magni distinctio ex eaque hic illum dolorum, quod quae delectus
+        architecto natus qui! Lorem ipsum dolor sit amet consectetur adipisicing
+        elit. Explicabo iure nihil quia odio quam quaerat voluptatibus
+        dignissimos nemo molestias eaque rem, sit molestiae cupiditate quae
+        quisquam doloremque in culpa. Perferendis? Lorem ipsum dolor sit amet,
+        consectetur adipisicing elit. Amet ut doloremque facere modi mollitia
+        adipisci voluptas eaque error animi alias corrupti, aliquid officia
+        natus perspiciatis nam vero. Voluptas, excepturi sapiente. Lorem ipsum
+        dolor sit amet consectetur, adipisicing elit. Porro unde voluptates
+        facere sint laboriosam beatae sequi ipsum magni distinctio ex eaque hic
+        illum dolorum, quod quae delectus architecto natus qui! Lorem ipsum
+        dolor sit amet consectetur adipisicing elit. Explicabo iure nihil quia
+        odio quam quaerat voluptatibus dignissimos nemo molestias eaque rem, sit
+        molestiae cupiditate quae quisquam doloremque in culpa. Perferendis?
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet ut
+        doloremque facere modi mollitia adipisci voluptas eaque error animi
+        alias corrupti, aliquid officia natus perspiciatis nam vero. Voluptas,
+        excepturi sapiente. Lorem ipsum dolor sit amet consectetur, adipisicing
+        elit. Porro unde voluptates facere sint laboriosam beatae sequi ipsum
+        magni distinctio ex eaque hic illum dolorum, quod quae delectus
+        architecto natus qui! Lorem ipsum dolor sit amet consectetur adipisicing
+        elit. Explicabo iure nihil quia odio quam quaerat voluptatibus
+        dignissimos nemo molestias eaque rem, sit molestiae cupiditate quae
+        quisquam doloremque in culpa. Perferendis? Lorem ipsum dolor sit amet,
+        consectetur adipisicing elit. Amet ut doloremque facere modi mollitia
+        adipisci voluptas eaque error animi alias corrupti, aliquid officia
+        natus perspiciatis nam vero. Voluptas, excepturi sapiente. Lorem ipsum
+        dolor sit amet consectetur, adipisicing elit. Porro unde voluptates
+        facere sint laboriosam beatae sequi ipsum magni distinctio ex eaque hic
+        illum dolorum, quod quae delectus architecto natus qui! Lorem ipsum
+        dolor sit amet consectetur adipisicing elit. Explicabo iure nihil quia
+        odio quam quaerat voluptatibus dignissimos nemo molestias eaque rem, sit
+        molestiae cupiditate quae quisquam doloremque in culpa. Perferendis?
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet ut
+        doloremque facere modi mollitia adipisci voluptas eaque error animi
+        alias corrupti, aliquid officia natus perspiciatis nam vero. Voluptas,
+        excepturi sapiente. Lorem ipsum dolor sit amet consectetur, adipisicing
+        elit. Porro unde voluptates facere sint laboriosam beatae sequi ipsum
+        magni distinctio ex eaque hic illum dolorum, quod quae delectus
+        architecto natus qui! Lorem ipsum dolor sit amet consectetur adipisicing
+        elit. Explicabo iure nihil quia odio quam quaerat voluptatibus
+        dignissimos nemo molestias eaque rem, sit molestiae cupiditate quae
+        quisquam doloremque in culpa. Perferendis? Lorem ipsum dolor sit amet,
+        consectetur adipisicing elit. Amet ut doloremque facere modi mollitia
+        adipisci voluptas eaque error animi alias corrupti, aliquid officia
+        natus perspiciatis nam vero. Voluptas, excepturi sapiente. Lorem ipsum
+        dolor sit amet consectetur, adipisicing elit. Porro unde voluptates
+        facere sint laboriosam beatae sequi ipsum magni distinctio ex eaque hic
+        illum dolorum, quod quae delectus architecto natus qui! Lorem ipsum
+        dolor sit amet consectetur adipisicing elit. Explicabo iure nihil quia
+        odio quam quaerat voluptatibus dignissimos nemo molestias eaque rem, sit
+        molestiae cupiditate quae quisquam doloremque in culpa. Perferendis?
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet ut
+        doloremque facere modi mollitia adipisci voluptas eaque error animi
+        alias corrupti, aliquid officia natus perspiciatis nam vero. Voluptas,
+        excepturi sapiente. Lorem ipsum dolor sit amet consectetur, adipisicing
+        elit. Porro unde voluptates facere sint laboriosam beatae sequi ipsum
+        magni distinctio ex eaque hic illum dolorum, quod quae delectus
+        architecto natus qui! Lorem ipsum dolor sit amet consectetur adipisicing
+        elit. Explicabo iure nihil quia odio quam quaerat voluptatibus
+        dignissimos nemo molestias eaque rem, sit molestiae cupiditate quae
+        quisquam doloremque in culpa. Perferendis? Lorem ipsum dolor sit amet,
+        consectetur adipisicing elit. Amet ut doloremque facere modi mollitia
+        adipisci voluptas eaque error animi alias corrupti, aliquid officia
+        natus perspiciatis nam vero. Voluptas, excepturi sapiente. Lorem ipsum
+        dolor sit amet consectetur, adipisicing elit. Porro unde voluptates
+        facere sint laboriosam beatae sequi ipsum magni distinctio ex eaque hic
+        illum dolorum, quod quae delectus architecto natus qui! Lorem ipsum
+        dolor sit amet consectetur adipisicing elit. Explicabo iure nihil quia
+        odio quam quaerat voluptatibus dignissimos nemo molestias eaque rem, sit
+        molestiae cupiditate quae quisquam doloremque in culpa. Perferendis?
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet ut
+        doloremque facere modi mollitia adipisci voluptas eaque error animi
+        alias corrupti, aliquid officia natus perspiciatis nam vero. Voluptas,
+        excepturi sapiente. Lorem ipsum dolor sit amet consectetur, adipisicing
+        elit. Porro unde voluptates facere sint laboriosam beatae sequi ipsum
+        magni distinctio ex eaque hic illum dolorum, quod quae delectus
+        architecto natus qui! Lorem ipsum dolor sit amet consectetur adipisicing
+        elit. Explicabo iure nihil quia odio quam quaerat voluptatibus
+        dignissimos nemo molestias eaque rem, sit molestiae cupiditate quae
+        quisquam doloremque in culpa. Perferendis?
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- app/src/components/common/NavBar.tsx
+++ app/src/components/common/NavBar.tsx
@@ -1,5 +1,25 @@
+import { AppBar, Toolbar, Typography, IconButton } from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+import { Container } from "@mui/system";
+import AbcIcon from "@mui/icons-material/Abc";
+import React from "react";
+
+//https://react.school/material-ui/appbar
+//For instruction and inspiration
+
 const NavBar = () => {
-  return <></>;
+  return (
+    <React.Fragment>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton edge="start" color="inherit">
+            <MenuIcon></MenuIcon>
+          </IconButton>
+          <Typography>NAME</Typography>
+        </Toolbar>
+      </AppBar>
+    </React.Fragment>
+  );
 };
 
 export default NavBar;
